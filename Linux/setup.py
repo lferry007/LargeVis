174f3d854950584294ea1fa50b678f6464bb80a7
Leland McInnes <leland.mcinnes@gmail.com>
2018-09-02 13:37:18 -0400

    Support py3 and loading numpy arrays

--- Linux/setup.py
+++ Linux/setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup, Extension
+import numpy as np
 
 LargeVis = Extension('LargeVis',
                     sources = ['LargeVis.cpp', 'LargeVismodule.cpp'],
                     depends=['LargeVis.h'],
-                    include_dirs = ['/usr/local/include'],
+                    include_dirs = ['/usr/local/include', np.get_include()],
                     library_dirs = ['/usr/local/lib'],
                     libraries=['gsl', 'gslcblas'],
                     extra_compile_args=['-lm -pthread -lgsl -lgslcblas -Ofast -march=native -ffast-math'])
