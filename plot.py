6e2ed7b2ed76bdbf25cda684c53a79f108219783
H@di <hadi@hadisafari.ir>
2021-02-19 17:47:03 +0330

    refactor python files

--- plot.py
+++ plot.py
@@ -1,13 +1,15 @@
+#!/usr/bin/env python
+
 import numpy
 import matplotlib.pyplot as plt
 import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-input', default = '', help = 'input file')
-parser.add_argument('-label', default = '', help = 'label file')
-parser.add_argument('-output', default = '', help = 'output file')
-parser.add_argument('-range', default = '', help = 'axis range')
+parser.add_argument('-input', default='', help='input file')
+parser.add_argument('-label', default='', help='label file')
+parser.add_argument('-output', default='', help='output file')
+parser.add_argument('-range', default='', help='axis range')
 
 args = parser.parse_args()
 
@@ -33,12 +35,9 @@ colors = plt.cm.rainbow(numpy.linspace(0, 1, len(all_data)))
 for color, ll in zip(colors, sorted(all_data.keys())):
     x = [t[0] for t in all_data[ll]]
     y = [t[1] for t in all_data[ll]]
-    plt.plot(x, y, '.', color = color, markersize = 1)
+    plt.plot(x, y, '.', color=color, markersize=1)
 if args.range != '':
     l = abs(float(args.range))
     plt.xlim(-l, l)
     plt.ylim(-l, l)
-plt.savefig(args.output, dpi = 500)
-
-
-
+plt.savefig(args.output, dpi=500)
