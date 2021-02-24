925852be0895438402687fdeb4098936a89aaef7
H@di <hadi@hadisafari.ir>
2021-02-25 01:44:16 +0330

    add annotation

--- plot.py
+++ plot.py
@@ -2,38 +2,55 @@
 
 import argparse
 
-import numpy
-import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import rcParams, pyplot as plt
+
+rcParams["svg.fonttype"] = "none"
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--input', '-i', default='', help='input file', required=True)
-parser.add_argument('--label', '-l', default='', help='label file')
-parser.add_argument('--output', '-o', default='', help='output file', required=True)
-parser.add_argument('--range', '-r', type=float, help='axis range')
-parser.add_argument('--no-axis', '-n', help='hide axis', action='store_true')
-parser.add_argument('--legend', '-s', help='show legend', action='store_true')
+parser.add_argument("--input", "-i", default="", help="input file", required=True)
+parser.add_argument("--output", "-o", default="", help="output file", required=True)
+parser.add_argument("--clusters", "-c", default="", help="clusters file")
+parser.add_argument("--labels", "-l", default="", help="labels to annotate file")
+parser.add_argument("--range", "-r", type=float, help="axis range")
+parser.add_argument("--no-axis", "-n", help="hide axis", action="store_true")
+parser.add_argument("--legend", "-s", help="show legend", action="store_true")
 args = parser.parse_args()
 
-labels = {}
-if args.label != '':
-    with open(args.label) as f:
+clusters = {}
+if args.clusters != "":
+    with open(args.clusters) as f:
         for line in f:
-            node_id, label = line.strip().split()
-            labels[node_id] = label
+            node, cluster = line.strip().split()
+            clusters[node] = cluster
 
-all_data = {}
+lables = []
+if args.labels != "":
+    with open(args.labels) as f:
+        lables = list(map(lambda line: line.strip(), f))
+
+positions_by_cluster = {}
+positions = {}
 with open(args.input) as f:
     _ = f.readline()  # ignore first line
     for line in f:
-        vec = line.strip().split(' ')
-        all_data.setdefault(labels.get(vec[0], ''), []).append((float(vec[-2]), float(vec[-1])))
+        vec = line.strip().split(" ")
+        node = vec[0]
+        pos = (float(vec[-2]), float(vec[-1]))
+        positions[node] = pos
+        positions_by_cluster.setdefault(clusters.get(node, ""), []).append(pos)
 
-colors = plt.cm.tab10(numpy.linspace(0, 1, len(all_data)))
+colors = plt.cm.tab10(np.linspace(0, 1, len(positions_by_cluster)))
 
-for color, label in zip(colors, sorted(all_data.keys())):
-    x = [t[0] for t in all_data[label]]
-    y = [t[1] for t in all_data[label]]
-    plt.plot(x, y, '.', color=color, markersize=1, label=label)
+for color, cluster in zip(colors, sorted(positions_by_cluster.keys())):
+    x = [t[0] for t in positions_by_cluster[cluster]]
+    y = [t[1] for t in positions_by_cluster[cluster]]
+    plt.plot(x, y, ".", color=color, markersize=1, label=cluster)
+
+
+for node in lables:
+    x, y = positions[node]
+    plt.annotate(node, xy=(x, y), xytext=(x - 3, y - 3), arrowprops=dict(arrowstyle="-"), fontsize="xx-small")
 
 if args.range:
     axis_limit = abs(float(args.range))
@@ -41,10 +58,10 @@ if args.range:
     plt.ylim(-axis_limit, axis_limit)
 
 if args.no_axis:
-    plt.axis('off')
+    plt.axis("off")
 
 if args.legend:
     plt.legend()
 
-plt.gca().set_aspect('equal', adjustable='box')
+plt.gca().set_aspect("equal", adjustable="box")
 plt.savefig(args.output, dpi=500)
